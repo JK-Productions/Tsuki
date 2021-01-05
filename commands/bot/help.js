212abf6e16397d784f3e138739b00e3dcc87def3
ChibiKev <k3v.ch3n@gmail.com>
2021-01-04 19:08:11 -0500

    Added outline file

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -6,16 +6,16 @@ module.exports = {
 	aliases: ['h','commands'],
 	usage: '[command name]',
 	execute(message, args) {
-		const data = [];
+	  const data = [];
 		const { commands } = message.client;
 
-		if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+    if (!args.length) {
+      data.push('Here\'s a list of all my commands:');
+      data.push(commands.map(command => command.name).join(', '));
+      data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-			return message.channel.send(data, { split: true })
-		}
+      return message.channel.send(data, { split: true })
+    }
 
 		const name = args[0].toLowerCase();
 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
